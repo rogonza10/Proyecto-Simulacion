b7fe5901e7d65afcdf757e047281ac2f90a4ecb2
GONZALO LOPEZ MACHADO <L21210390@tijuana.tecnm.mx>
2022-11-30 11:37:26 -0800

    Nombre gonzalillo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,13 +31,14 @@ namespace Proyecto_simulacion
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.DarkRed;
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button1.Location = new System.Drawing.Point(212, 81);
+            this.button1.Location = new System.Drawing.Point(212, 128);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(378, 233);
             this.button1.TabIndex = 0;
@@ -47,16 +48,27 @@ namespace Proyecto_simulacion
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(281, 338);
+            this.textBox1.Location = new System.Drawing.Point(265, 383);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(252, 22);
             this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(107, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(575, 106);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "GONZALILLO";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -70,6 +82,7 @@ namespace Proyecto_simulacion
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
